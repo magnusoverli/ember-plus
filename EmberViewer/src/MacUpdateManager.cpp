d5d6dffbdc962539bbe7576cd59561a509c99525
magnusoverli <magnus.overli@gmail.com>
2025-10-27 08:44:36 +0100

    Add missing QTimer includes in Windows and Mac update managers

--- EmberViewer/src/MacUpdateManager.cpp
+++ EmberViewer/src/MacUpdateManager.cpp
@@ -14,6 +14,7 @@
 #include <QFile>
 #include <QProcess>
 #include <QCoreApplication>
+#include <QTimer>
 #include <QDebug>
 #include <QDir>
 
--- EmberViewer/src/WindowsUpdateManager.cpp
+++ EmberViewer/src/WindowsUpdateManager.cpp
@@ -14,6 +14,7 @@
 #include <QFile>
 #include <QProcess>
 #include <QCoreApplication>
+#include <QTimer>
 #include <QDebug>
 
 WindowsUpdateManager::WindowsUpdateManager(QObject *parent)
