640b2d8a16e4fc1e68720f26a181a3d8cd3cc0b4
magnusoverli <magnus.overli@gmail.com>
2025-10-31 16:31:33 +0100

    Update tests to remove relocated constants

--- EmberViewer/tests/test_constants.cpp
+++ EmberViewer/tests/test_constants.cpp
@@ -10,8 +10,6 @@ private slots:
     void testMainWindowConstants()
     {
         QCOMPARE(MainWindow::MATRIX_LABEL_PATH_MARKER, 666999666);
-        QCOMPARE(MainWindow::ACTIVITY_TIMEOUT_MS, 60000);
-        QCOMPARE(MainWindow::TICK_INTERVAL_MS, 1000);
         QCOMPARE(MainWindow::DEFAULT_EMBER_PORT, 9092);
         QCOMPARE(MainWindow::DEFAULT_PORT_FALLBACK, 9000);
     }
@@ -23,21 +21,9 @@ private slots:
     
     void testConstantsArePositive()
     {
-        QVERIFY(MainWindow::ACTIVITY_TIMEOUT_MS > 0);
-        QVERIFY(MainWindow::TICK_INTERVAL_MS > 0);
         QVERIFY(MainWindow::DEFAULT_EMBER_PORT > 0);
         QVERIFY(MainWindow::DEFAULT_EMBER_PORT < 65536); // Valid port range
         QVERIFY(EmberConnection::CONNECTION_TIMEOUT_MS > 0);
-    }
-    
-    void testTimeoutRelationships()
-    {
-        // Activity timeout should be multiple of tick interval
-        QVERIFY(MainWindow::ACTIVITY_TIMEOUT_MS % MainWindow::TICK_INTERVAL_MS == 0);
-        
-        // Activity timeout should be reasonable (between 10s and 5min)
-        QVERIFY(MainWindow::ACTIVITY_TIMEOUT_MS >= 10000);
-        QVERIFY(MainWindow::ACTIVITY_TIMEOUT_MS <= 300000);
     }
 };
 
